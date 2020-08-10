2fa230168392469c4aafdb5a1b1cd070550dea87
Alexander Malykh <fulgrimka1@gmail.com>
2020-08-10 11:29:07 +0300

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import Rollbar from 'rollbar';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT || 5000;
-
 const app = new fastify({
   logger: true
 });
-/*
+
 const rollbar = new Rollbar({
   accessToken: process.env.POST_SERVER_ITEM_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true
 });
-*/
+
 //app.use(rollbar.errorHandler());
 
 rollbar.log("Hello world!");
