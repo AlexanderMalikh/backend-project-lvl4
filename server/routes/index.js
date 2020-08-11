5debf4dc8f2b515e33c6042313aa57bec8a65da2
Alexander Malykh <fulgrimka1@gmail.com>
2020-08-11 15:52:18 +0300

    login form

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,5 @@
 export default async (fastify) => {
-  fastify.get('/', async (request, reply) => reply.view('server/views/layouts/main'));
+  fastify.get('/', async (request, reply) => reply.view('server/views/welcome/welcome'));
+  fastify.get('/users', async (request, reply) => reply.view('server/views/users/users'));
+  fastify.get('/users/new', async (request, reply) => reply.view('server/views/users/new'));
 };
